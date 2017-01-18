ea58cfaaaef984a40550b21b744f587bafe93a6e
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-01-18 11:37:31 +0200

    Implemented a simple FizzBuzzer.

---
+++ FizzBuzzer/FizzBuzzerSimple.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzer
+{
+    class FizzBuzzerSimple
+    {
+        public void FizzBuzz(int number)
+        {
+            if (InRange(number))
+            {
+                if (IsFizzBuzz(number))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (IsBuzz(number))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (IsFizz(number))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(number);
+                }     
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+
+        private bool InRange(int number)
+        {
+            if (number >= 1 && number <= 100)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsFizz(int number)
+        {
+            if (number % 4 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsBuzz(int number)
+        {
+            if (number % 7 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsFizzBuzz(int number)
+        {
+            if (IsBuzz(number) && IsFizz(number))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
