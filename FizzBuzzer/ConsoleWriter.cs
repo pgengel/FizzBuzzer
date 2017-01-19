a9f4b6cd807fd82e0382badd81cc78e5bc55efb9
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-01-19 09:04:57 +0200

    setting up the OOP  for javascript

--- FizzBuzzer/ConsoleWriter.cs
+++ FizzBuzzer/ConsoleWriter.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzzer
 {
@@ -10,35 +6,35 @@ namespace FizzBuzzer
 
     public interface IWriter
     {
-        string ConsoleWrite(IConsoleWriter consoleWrite);
+        void ConsoleWrite(IConsoleWriter consoleWrite);
     }
 
     public interface IConsoleWriter
     {
-        string Write(string format, object[] args);
-        string WriteLine(string format, object[] args);
+        void Write(string format, object[] args);
+        void WriteLine(string format, object[] args);
     }
 
-    public class Writer: IWriter
+    public class Writer : IWriter
     {
 
-        public string ConsoleWrite(IConsoleWriter _consoleWriter)
+        public void ConsoleWrite(IConsoleWriter consoleWriter)
         {
-            throw new NotImplementedException();
+            //_consoleWriter.WriteLine();
         }
 
     }
 
     public class ConsoleWriter : IConsoleWriter
     {
-        public string Write(string format, object[] args)
+        public void Write(string format, object[] args)
         {
-            throw new NotImplementedException();
+            Console.Write(format, args);
         }
 
-        public string WriteLine(string format, object[] args)
+        public void WriteLine(string format, object[] args)
         {
-            throw new NotImplementedException();
+            Console.WriteLine(format, args);
         }
     }
 }
